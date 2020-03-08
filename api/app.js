3de0bf27456d2151f136f5f6a8ca19465b7b24c0
Leyrus <victor.leyrus@gmail.com>
2020-03-08 22:47:40 +0300

    AV-89

--- api/app.js
+++ api/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const morgan = require('morgan')
 const cors = require('cors');
 const routerUser = require('./routes/user');
 const routerMessages = require('./routes/messages');
@@ -10,6 +12,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(__dirname + '/log/access.log',{flags: 'a'});
+
+// setup the logger
+app.use(morgan('combined', {stream: accessLogStream}))
 
 app.use('/user', routerUser);
 app.use('/messages', routerMessages);
