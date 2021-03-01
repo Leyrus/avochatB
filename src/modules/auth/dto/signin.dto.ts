cf6eedd3f64f95d10047791c1298aa22a5c61ed4
Victor Plonin <plonin.va@esphere.ru>
2021-03-01 08:05:20 +0300

    AV-128: added examples

--- src/modules/auth/dto/signin.dto.ts
+++ src/modules/auth/dto/signin.dto.ts
@@ -12,5 +12,5 @@ export class SignInDto {
     /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/,
     { message: 'Weak password' },
   )
-  password: string;
+  password?: string;
 }
